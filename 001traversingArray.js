5ccbb9ea8614900b6d7195e84e3fa78a984c0d35
UchuNoKen <xen.o.fite@gmail.com>
2019-04-01 12:25:33 -0400

    traversing arrays

--- 001traversingArray.js
+++ 001traversingArray.js
@@ -141,3 +141,72 @@ function newReleases() {
     return { id: video.id, title: video.title };
   });
 }
+
+// Filtering Arrays -----------------------------------------------------------------
+// - using forEach(), pass a test to each item, collect the items that pass the test into a new array
+
+// Ex. 6
+// - loop through newReleases, add videos with 5.0 to the videos array
+
+function rating5() {
+  var newReleases = [
+      {
+        id: 70111470,
+        title: "Die Hard",
+        boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 4.0,
+        bookmark: []
+      },
+      {
+        id: 654356453,
+        title: "Bad Boys",
+        boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 5.0,
+        bookmark: [{ id: 432534, time: 65876586 }]
+      },
+      {
+        id: 65432445,
+        title: "The Chamber",
+        boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 4.0,
+        bookmark: []
+      },
+      {
+        id: 675465,
+        title: "Fracture",
+        boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 5.0,
+        bookmark: [{ id: 432534, time: 65876586 }]
+      }
+    ],
+    videos = [];
+
+  newReleases.forEach(video => {
+    if (video.rating === 5.0) {
+      videos.push(video);
+    }
+  });
+
+  return videos;
+}
+
+// Ex. 7 Implement Filter
+// - add filter() to the Array type.
+// - filter accepts a predicate
+// - predicate returns a boolean indicating if the array item should be retained
+
+Array.prototype.filter = function(predicateFunction) {
+  let results = [];
+  this.forEach(itemInArray => {
+    if (predicateFunction(itemInArray)) {
+      results.push(itemInArray);
+    }
+  });
+  return results;
+};
+
+// JSON.stringify([1,2,3].filter(function(x) { return x > 2})) === "[3]";  // true
